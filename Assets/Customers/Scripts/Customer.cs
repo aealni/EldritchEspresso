0ca54a1992b0f3c96960c69193105b07057de0c0
ddai20 <ddai20@illinois.edu>
2025-09-25 21:24:45 -0500

    customer manager untested

--- Assets/Customers/Scripts/Customer.cs
+++ Assets/Customers/Scripts/Customer.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Customer : MonoBehaviour
@@ -17,33 +15,18 @@ public class Customer : MonoBehaviour
     [Header("Movement")]
 
     [SerializeField] float movement_speed = 10;
-    [SerializeField] Vector2Int entrance_pos = new Vector2Int(-1, -1);
-    [SerializeField] Vector2Int curr_pos = new Vector2Int(-1, -1);
-    [SerializeField] Vector2Int target_pos = new Vector2Int(-1,-1);
-    [SerializeField] Vector2Int next_move_pos = new Vector2Int(-1,-1);
-
-    [Header("Grid")]
+    [SerializeField] Vector2Int curr_pos = new(-1, -1);
+    [SerializeField] Vector2Int target_pos = new(-1, -1);
+    [SerializeField] Vector2Int next_move_pos = new(-1, -1);
 
     // [x, y]
     // 0 is empty
     // -1 is seats
+    // -2 are spawn locations
     // 1 are obstacles
     // else: id of customer
-    [SerializeField]
-    static int[,] grid = {
-        {0, 1, 0, 0, 0, 0, 0},
-        {0, 1, 0, 1, 0, 0, 0},
-        {0, 1, 0, 1, 0, 0, 0},
-        {0, 1, 0, 1, 0, 0, 0},
-        {0, 1, 0, 1, 0, 0, 0},
-        {0, 0, 0, 1, 0, 0, -1}
-    };
-
-    [SerializeField] Vector2Int grid_offset = Vector2Int.zero; // Bottom Left Corner
-    [SerializeField] int grid_square_size = 1;
 
     //true = available, false = not 
-    [SerializeField] Dictionary<Vector2Int, bool> seats = new();
 
     // State
     enum STATE
@@ -59,56 +42,15 @@ public class Customer : MonoBehaviour
     [SerializeField] List<DummyFood> menu;
     [SerializeField] DummyFood order;
 
-    static int id_count = 2;
+    [SerializeField] int id;
 
-    [SerializeField] int curr_id;
-
-    void Start()
+    public void Activate(int new_id, Vector2Int spawn_pos, Vector2Int seat)
     {
-        // Entrance needs to be passed in before init of customer currently
-        grid[entrance_pos.x, entrance_pos.y] = 0;
-        curr_pos = entrance_pos;
-        curr_id = id_count++;
-        transform.position = GridToWorld(curr_pos);
-        
-        ParseGrid();
-        Seating();
-    }
+        id = new_id;
+        curr_pos = spawn_pos;
+        target_pos = seat;
 
-    void ParseGrid()
-    {
-        for (int x = 0; x < grid.GetLength(0); x++) //Rows
-        {
-            for (int y = 0; y < grid.GetLength(1); y++) //Cols
-            {
-                if (grid[x, y] == -1) //Mark seats
-                {
-                    seats.Add(new Vector2Int(x, y), true);
-                }
-            }
-        }
-    }
-
-    void Seating()
-    {
-        state = STATE.SEATING;   
-        List<Vector2Int> available_seats = new();
-        foreach (KeyValuePair<Vector2Int, bool> seat in seats)
-        {
-            if (seat.Value)
-            {
-                available_seats.Add(seat.Key);
-            }
-        }
-
-        if (available_seats.Count == 0)
-        {
-            // Wait for seat TODO
-            Debug.LogError("No seats. Implement Waiting for seats.");
-        }
-
-        target_pos = available_seats[UnityEngine.Random.Range(0, available_seats.Count)];
-        seats[target_pos] = false;
+        state = STATE.SEATING;
     }
 
     void Ordering()
@@ -124,7 +66,7 @@ public class Customer : MonoBehaviour
         timer = ordering_patience;
     }
 
-    void TakeOrder()
+    public void TakeOrder()
     {
         Waiting();
     }
@@ -149,35 +91,7 @@ public class Customer : MonoBehaviour
     void Leaving()
     {
         state = STATE.LEAVING;
-        target_pos = entrance_pos;
-        seats[curr_pos] = true;
-    }
-
-    // TODO
-    Vector2 GridToWorld(Vector2Int pos)
-    {
-        return (pos * grid_square_size) + grid_offset + Vector2.one * (grid_square_size / 2F);
-    }
-
-    // TODO
-    Vector2Int WorldToGrid(Vector2 pos)
-    {
-        Vector2 swp = pos - (grid_offset + Vector2.one * (grid_square_size / 2F));
-        return new Vector2Int((int)swp.x, (int)swp.y) / grid_square_size;
-    }
-
-    // TODo
-    void UpdateGridPosition()
-    {
-        Vector2Int grid_pos = WorldToGrid(transform.position);
-
-        // Edits the grid wrong currently. Messes up placed obstacles.
-        // grid[curr_pos.x, curr_pos.y] = 0;
-
-        
-        // grid[grid_pos.x, grid_pos.y] = curr_id;
-        curr_pos = grid_pos;
-
+        target_pos = CustomerManager.FindEntrance();
     }
 
     // TODO
@@ -185,89 +99,17 @@ public class Customer : MonoBehaviour
     {
         if (next_move_pos != new Vector2Int(-1, -1))
         {
-            transform.position = Vector2.MoveTowards(transform.position, GridToWorld(next_move_pos), movement_speed * Time.deltaTime);
-            if ((GridToWorld(next_move_pos) - (Vector2)transform.position).magnitude > 0.01)
+            transform.position = Vector2.MoveTowards(transform.position, CustomerManager.GridToWorld(next_move_pos), movement_speed * Time.deltaTime);
+            if ((CustomerManager.GridToWorld(next_move_pos) - (Vector2)transform.position).magnitude > 0.01)
             {
                 return;
             }
+
+            curr_pos = CustomerManager.UpdateGridPosition(curr_pos, next_move_pos, id);
         }
+
         //Astar
-        next_move_pos = Astar();
-    }
-
-    Vector2Int Astar()
-    {
-        int sort_func(Tuple<Vector2Int, float> a, Tuple<Vector2Int, float> b)
-        {
-            int x1 = a.Item2.CompareTo(b.Item2);
-            int x2 = a.Item1.x.CompareTo(b.Item1.x);
-            int x3 = a.Item1.y.CompareTo(b.Item1.y);
-
-            if (x1 == 0)
-            {
-                if (x2 == 0)
-                {
-                    return x3;
-                }
-
-                return x2;
-            }
-
-            return x1;
-        }
-
-        Dictionary<Vector2Int, Vector2Int> parent_lookup = new(){ {curr_pos, curr_pos } };
-        Dictionary<Vector2Int, float> path_length = new() { {curr_pos, 0 } };
-        SortedSet<Tuple<Vector2Int, float>> heap = new(Comparer<Tuple<Vector2Int, float>>.Create(sort_func)) { new Tuple<Vector2Int, float>(curr_pos, 0) };
-
-        while (heap.Count != 0)
-        {
-            Tuple<Vector2Int, float> curr = heap.First();
-            heap.Remove(curr);
-
-            if (curr.Item1 == target_pos) //Reach end
-            {
-                break;
-            }
-
-            // Add all 8
-            for (int i = -1; i <= 1; i++)
-            {
-                for (int j = -1; j <= 1; j++)
-                {
-                    Vector2Int child = new Vector2Int(i, j) + curr.Item1;
-                    if (parent_lookup.ContainsKey(child))
-                    {
-                        continue;
-                    }
-                    if (child.x < 0 || child.y < 0 || child.x >= grid.GetLength(0) || child.y >= grid.GetLength(1) || (grid[child.x,child.y] > 0 && grid[child.x,child.y] < curr_id))
-                    {
-                        continue;
-                    }
-
-                    path_length[child] = path_length[curr.Item1] + 1;
-                    parent_lookup[child] = curr.Item1;
-                    heap.Add(new(child, path_length[child] + (target_pos - child).magnitude));
-                }
-            }
-        }
-
-        List<Vector2Int> path = new();
-        Vector2Int backtrack = target_pos;
-        if (!parent_lookup.ContainsKey(target_pos))
-        {
-            Debug.Log("No path found");
-            return new Vector2Int(-1,-1);
-        }
-
-        while (backtrack != curr_pos)
-        {
-            path.Add(backtrack);
-            backtrack = parent_lookup[backtrack];
-        }
-        Debug.Log(path[path.Count - 1]);
-        
-        return path[path.Count - 1];
+        next_move_pos = CustomerManager.Astar(curr_pos, target_pos, id);
     }
 
     // Update is called once per frame
@@ -282,18 +124,19 @@ public class Customer : MonoBehaviour
         {
             if (curr_pos == target_pos)
             {
-                target_pos = new Vector2Int(-1, -1);
                 if (state == STATE.SEATING)
                 {
                     Ordering();
                 }
                 else if (state == STATE.LEAVING)
                 {
-                    //Kill self
+                    Destroy(gameObject);
                 }
             }
-            Move();
-            UpdateGridPosition();
+            else
+            {
+                Move();   
+            }
         }
     }
 }
---
+++ Assets/Customers/Scripts/CustomerManager.cs
@@ -0,0 +1,266 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CustomerManager : MonoBehaviour
+{
+    public static GameObject customer_prefab;
+    static readonly float spawn_delay;
+    static float timer;
+
+    static int[,] grid;
+    static readonly Vector2Int grid_offset = Vector2Int.zero; // Bottom Left Corner
+    static readonly int grid_square_size = 1;
+    static readonly int padding = 2;
+    static List<Vector2Int> seats = new();
+    static readonly List<Vector2Int> entrances = new();
+
+    static List<Vector2Int> spawn_locations = new();
+
+
+    static int id_count = 2;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        CreateGrid();
+        ParseGrid();
+    }
+
+    void SpawnCustomer()
+    {
+        Vector2Int new_spawn_pos = FindSpawnLocation();
+
+        if (new_spawn_pos == new Vector2Int(-1, -1)) {
+            return;
+        }
+
+        GameObject new_customer = Instantiate(customer_prefab);
+        Customer new_customer_script = new_customer.GetComponent<Customer>();
+
+        int new_id = id_count++;
+
+        grid[new_spawn_pos.x, new_spawn_pos.y] = new_id;
+
+        new_customer_script.Activate(new_id, new_spawn_pos, FindSeat());
+    }
+
+    Vector2Int FindSpawnLocation() {
+        spawn_locations = spawn_locations.OrderBy(x => UnityEngine.Random.value).ToList();
+
+        for (int i = 0; i < spawn_locations.Count(); i++)
+        {
+            Vector2Int pos = spawn_locations[i];
+            if (grid[pos.x, pos.y] == 0)
+            {
+                return pos;
+            }
+        }
+
+        return new Vector2Int(-1, -1);
+    }
+
+    Vector2Int FindSeat()
+    {
+        if (seats.Count == 0)
+        {
+            // Wait for seat TODO
+            Debug.LogError("No seats. Implement Waiting for seats.");
+        }
+
+        Vector2Int ans = seats[UnityEngine.Random.Range(0, seats.Count)];
+        seats.Remove(ans);
+        return ans;
+    }
+
+    public static Vector2Int FindEntrance()
+    {
+        return entrances[UnityEngine.Random.Range(0, entrances.Count)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawn_delay)
+        {
+            SpawnCustomer();
+            timer = 0;
+        }
+
+        print_grid(grid);
+    }
+
+    public static Vector2 GridToWorld(Vector2Int pos)
+    {
+        return (pos * grid_square_size) + grid_offset + Vector2.one * (grid_square_size / 2F);
+    }
+
+    // TODO
+    public static Vector2Int WorldToGrid(Vector2 pos)
+    {
+        Vector2 swp = pos - (grid_offset + Vector2.one * (grid_square_size / 2F));
+        return new Vector2Int((int)swp.x, (int)swp.y) / grid_square_size;
+    }
+
+    public static Vector2Int UpdateGridPosition(Vector2Int curr_pos, Vector2Int next_pos, int curr_id)
+    {
+        // Edits the grid wrong currently. Messes up placed obstacles.
+        grid[curr_pos.x, curr_pos.y] = 0;
+
+
+        grid[next_pos.x, next_pos.y] = curr_id;
+
+        return next_pos;
+    }
+
+    public static Vector2Int Astar(Vector2Int start, Vector2Int target, int id)
+    {
+        if (start == target)
+        {
+            return target;
+        }
+
+        int sort_func(Tuple<Vector2Int, float> a, Tuple<Vector2Int, float> b)
+        {
+            int x1 = a.Item2.CompareTo(b.Item2);
+            int x2 = a.Item1.x.CompareTo(b.Item1.x);
+            int x3 = a.Item1.y.CompareTo(b.Item1.y);
+
+            if (x1 == 0)
+            {
+                if (x2 == 0)
+                {
+                    return x3;
+                }
+
+                return x2;
+            }
+
+            return x1;
+        }
+
+        Dictionary<Vector2Int, Vector2Int> parent_lookup = new() { { start, start } };
+        Dictionary<Vector2Int, float> path_length = new() { { start, 0 } };
+        SortedSet<Tuple<Vector2Int, float>> heap = new(Comparer<Tuple<Vector2Int, float>>.Create(sort_func)) { new Tuple<Vector2Int, float>(start, 0) };
+
+        while (heap.Count != 0)
+        {
+            Tuple<Vector2Int, float> curr = heap.First();
+            heap.Remove(curr);
+
+            if (curr.Item1 == target) //Reach end
+            {
+                break;
+            }
+
+            // Add all 8
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    Vector2Int child = new Vector2Int(i, j) + curr.Item1;
+                    if (parent_lookup.ContainsKey(child))
+                    {
+                        continue;
+                    }
+                    if (child.x < 0 || child.y < 0 || child.x >= grid.GetLength(0) || child.y >= grid.GetLength(1) || (grid[child.x, child.y] > 0 && grid[child.x, child.y] < id))
+                    {
+                        continue;
+                    }
+
+                    path_length[child] = path_length[curr.Item1] + 1;
+                    parent_lookup[child] = curr.Item1;
+                    heap.Add(new(child, path_length[child] + (target - child).magnitude));
+                }
+            }
+        }
+
+        List<Vector2Int> path = new();
+        Vector2Int backtrack = target;
+        if (!parent_lookup.ContainsKey(target))
+        {
+            Debug.Log("No path found");
+            return start;
+        }
+
+        while (backtrack != start)
+        {
+            path.Add(backtrack);
+            backtrack = parent_lookup[backtrack];
+        }
+
+        Debug.Log(path[path.Count - 1]);
+
+        return path[path.Count - 1];
+    }
+
+    public static void CreateGrid()
+    {
+        int[,] data = {
+            {1, 1, 1, -2, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1},
+            {1, 1, 1, -2, 1, 1, 1, 1, 1, 1, 1},
+        };
+
+        grid = new int[data.GetLength(0) + 2 * padding, data.GetLength(1) + 2 * padding];
+
+        for (int i = 0; i < data.GetLength(0); i++)
+        {
+            for (int j = 0; j < data.GetLength(1); j++)
+            {
+                grid[i + padding, j + padding] = data[i, j];
+            }
+        }
+    }
+
+    public static void ParseGrid()
+    {
+        for (int x = 0; x < grid.GetLength(0); x++) //Rows
+        {
+            for (int y = 0; y < grid.GetLength(1); y++) //Cols
+            {
+                Vector2Int pos = new(x, y);
+                if (grid[x, y] == -1) //Mark seats
+                {
+                    seats.Add(pos);
+                }
+
+                if (grid[x, y] == -2)
+                {
+                    entrances.Add(pos);
+                }
+
+                if ((x < padding || x >= (grid.GetLength(0) - padding)) && (y < padding || y >= (grid.GetLength(1) - padding)))
+                {
+                    spawn_locations.Add(pos);
+                }
+            }
+        }
+    }
+    
+    void print_grid(int[,] grid)
+    {
+        string s = "";
+        for (int i = grid.GetLength(1) - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < grid.GetLength(0); j++)
+            {
+                s += grid[j, i] + "\t";
+            }
+
+            s += "\n";
+        }
+
+        Debug.Log(s);
+    }
+}
