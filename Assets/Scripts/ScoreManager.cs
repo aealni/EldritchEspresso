5a210e44016211b1eb27ebb9668409ea6af01bd6
TomKim2003 <112648853+TomKim2003@users.noreply.github.com>
2025-10-02 19:44:01 -0500

    Update ScoreManager.cs

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,6 +5,34 @@ public class ScoreManager : MonoBehaviour
 {
   public static ScoreManager Instance { get; private set; }
 
+  private int currentScore = 0;
+
   public event Action<int> OnScoreChanged;
   public event Action<int> OnHighScoreChanged;
+
+  void Awake() 
+  {
+    if (Instance != null && Instance != this) 
+    {
+      Destroy(GameObject);
+      return;
+    }
+    Instance = this;
+    DontDestroyOnLoad(GameObject);
+  }
+  
+
+  public void addScore(int amount) 
+    {
+      currentScore += amount;
+    }
+
+  public void ResetScore()
+    {
+      currentScore = 0;
+    }
+  public void getCurrentScore()
+    {
+      return currentScore;
+    }
 }
