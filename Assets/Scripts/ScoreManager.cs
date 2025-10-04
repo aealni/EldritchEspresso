18cff11a3098e7c13abdda82bf4393941c8104ac
TomKim2003 <112648853+TomKim2003@users.noreply.github.com>
2025-10-03 22:19:15 -0500

    Update ScoreManager.cs
    Uptaded how score increase by several mini-games and reset score function after day finish. After day finish, showing day high score.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,38 +1,96 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-  public static ScoreManager Instance { get; private set; }
+    public static ScoreManager Instance { get; private set; }
 
-  private int currentScore = 0;
+    private readonly Dictionary<string, int> miniGameScores = new();
+    private int currentScore;
+    private int dayHighScore;
 
-  public event Action<int> OnScoreChanged;
-  public event Action<int> OnHighScoreChanged;
+    public event Action<int> OnScoreChanged;
+    public event Action<int> OnDayHighScoreChanged;
+    public event Action<int> OnDayFinished;
 
-  void Awake() 
-  {
-    if (Instance != null && Instance != this) 
+    private void Awake()
     {
-      Destroy(gameObject);
-      return;
-    }
-    Instance = this;
-    DontDestroyOnLoad(gameObject);
-  }
-  
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
 
-  public void addScore(int amount) 
-    {
-      currentScore += amount;
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-  public void ResetScore()
+    public IReadOnlyDictionary<string, int> MiniGameScores => miniGameScores;
+    public int CurrentScore => currentScore;
+    public int DayHighScore => dayHighScore;
+
+    public void AddMiniGameScore(string miniGameId, int amount)
     {
-      currentScore = 0;
+        if (string.IsNullOrWhiteSpace(miniGameId))
+        {
+            Debug.LogWarning("ScoreManager.AddMiniGameScore called with an empty miniGameId.");
+            return;
+        }
+
+        if (amount <= 0)
+        {
+            Debug.LogWarning($"ScoreManager.AddMiniGameScore ignored non-positive amount {amount} for mini-game '{miniGameId}'.");
+            return;
+        }
+
+        if (miniGameScores.TryGetValue(miniGameId, out var existingScore))
+        {
+            miniGameScores[miniGameId] = existingScore + amount;
+        }
+        else
+        {
+            miniGameScores.Add(miniGameId, amount);
+        }
+
+        ApplyScoreDelta(amount);
     }
-  public int getCurrentScore()
+
+    public void EndDay()
     {
-      return currentScore;
+        OnDayFinished?.Invoke(dayHighScore);
+        StartNewDay();
+    }
+
+    public void StartNewDay()
+    {
+        currentScore = 0;
+        dayHighScore = 0;
+        miniGameScores.Clear();
+
+        OnScoreChanged?.Invoke(currentScore);
+        OnDayHighScoreChanged?.Invoke(dayHighScore);
+    }
+
+    public void ResetScore()
+    {
+        StartNewDay();
+    }
+
+    public int GetCurrentScore()
+    {
+        return currentScore;
+    }
+
+    private void ApplyScoreDelta(int amount)
+    {
+        currentScore += amount;
+        OnScoreChanged?.Invoke(currentScore);
+
+        if (currentScore > dayHighScore)
+        {
+            dayHighScore = currentScore;
+            OnDayHighScoreChanged?.Invoke(dayHighScore);
+        }
     }
 }
